2f9a454648bc79465a1ceddbd6e2a6d3f3894ace
pham quoc toan <toanpqqe170045@fpt.edu.vn>
2024-10-15 19:34:16 +0700

    UI - home page, components

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='Header-contents'>
+        <h2>Order your favorite food here</h2>
+        <p>Enjoy the finest dishes made with fresh ingredients, crafted to perfection for food lovers like you!</p>
+        <button>View Menu</button> 
+      </div>
+    </div>
+  )
+}
+
+export default Header
